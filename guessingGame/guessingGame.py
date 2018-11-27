2d7494281a7895e4afca4cf38dde1120c6a5f8f6
Chase Porter <chaseporter@chases-mbp.lan>
2018-11-28 06:54:09 +1000

    This is my guessing game

--- guessingGame/guessingGame.py
+++ guessingGame/guessingGame.py
@@ -2,19 +2,22 @@ import random
 
 num = random.randint(1, 100)
 
+#DEBUG
+#print num
 
 print ("Guess a number between 1 and 100")
 
-while True:
+guess = input()
+i = int(guess)
 
-    guess = input()
+while i != num:
 
-    i = int(guess)
-
-    if i == num:
-        print("You guessed right")
-    elif i < num:
+    if i < num:
         print('Try higher')
     elif i > num:
         print('Try lower')
 
+    guess = input()
+    i = int(guess)
+
+print('You have guessed the correct number congratulations')
